9d32164683fa362cdc518dc459e150212d5a7781
zerlinazl <76027329+zerlinazl@users.noreply.github.com>
2025-05-09 17:23:58 -0700

    re-organize functions

---
+++ import_data_from_roboflow.py
@@ -0,0 +1,217 @@
+from roboflow import Roboflow
+from pycocotools import mask as mask_utils
+
+import os
+import cv2
+import json
+from collections import defaultdict
+import numpy as np
+
+def list_all_labels(coco_path):
+    """ 
+    Inputs: 
+        coco_path: path to COCO annotation file containing segmentation info.
+    Returns:
+        class_ids: list of integers representing each unique class in the segmentations.
+    """
+    with open(coco_path, 'r') as f:
+        coco = json.load(f)
+
+    # Get all class ids from segmentation annotations
+    class_ids = {ann['category_id'] for ann in coco['annotations']}
+
+    # Count unique classes
+    num_classes = len(class_ids)
+    print(f"Total number of unique classes: {num_classes}")
+
+    return class_ids
+
+def list_all_images(folder_path):
+    """
+    Inputs:
+        folder_path: path to folder containing images to list
+    Returns:
+        image_names: list of filenames ending in png, jpg, jpeg, tif, or tiff, in sorted order.
+    """
+
+    image_names = [
+            p for p in os.listdir(folder_path)
+            if os.path.splitext(p)[-1] in [".jpg", ".jpeg", ".JPG", ".JPEG", ".png", ".PNG", ".tif", ".tiff", ".TIF", ".TIFF"]
+        ]
+    image_names = list(sorted(image_names))
+    return image_names
+
+def get_keyframe_indices(coco_path, images_to_segment_path, class_id):
+    """
+    Gets the indices of the annotated segmentations wrt the image stack to segment.
+    Inputs:
+        coco_path: path to the COCO-format annotation JSON file.
+        images_to_segment_path: path to directory containing image sequence.
+        class_id: COCO category id to return keyframe indices for.
+    Returns:
+        keyframe_indices: Indices of images in the sequence that have annotations for the specified class.
+    """
+    with open(coco_path, 'r') as f:
+        coco = json.load(f)
+
+    ann_ids = sorted(list({ann["image_id"] for ann in coco["annotations"] if ann['category_id'] == class_id}))
+
+    keyframe_filenames = [
+        img['file_name'].split(".")[0] if 'file_name' in img else img['name']
+        for img in coco['images']
+        if img['id'] in ann_ids
+    ]
+
+    frame_names = list_all_images(images_to_segment_path)
+
+    keyframe_indices = {0, len(frame_names)-1}
+    for ann in keyframe_filenames:
+        for i, name in enumerate(frame_names):
+            if ann in name:
+                keyframe_indices.add(i)
+
+    keyframe_indices = sorted(list(keyframe_indices))
+    return keyframe_indices
+
+def clahe_normalize(image):
+    """
+    CLAHE normalization helps improve contrast for segmentation.
+    Inputs:
+        image: an image as array
+    Returns:
+        image_clahe: normalized image
+    """
+
+    # Convert to LAB color space
+    lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+    l, a, b = cv2.split(lab)
+
+    # Apply CLAHE to the L-channel
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    l_clahe = clahe.apply(l)
+
+    # Merge the channels and convert back to BGR
+    lab_clahe = cv2.merge((l_clahe, a, b))
+    image_clahe = cv2.cvtColor(lab_clahe, cv2.COLOR_LAB2BGR)
+
+    return image_clahe
+
+def init_from_roboflow(workspace_name, project_name, api_key):
+    """
+    Downloads the project data from roboflow.
+
+    Inputs:
+        workspace_name: name of the Roboflow workspace.
+        project_name: name of the Roboflow project.
+        api_key: API key
+    Returns:
+        COCO_PATH: path to annotations file
+        class_ids: a list of integers representing each unique class in the dataset segmentations
+    """
+    rf = Roboflow(api_key=api_key)
+    project = rf.workspace(workspace_name).project(project_name)
+
+    # Automatically download the latest version
+    versions = project.versions()
+    latest_version = max(versions, key=lambda v: v.version)
+    dataset = latest_version.download("coco-segmentation")
+
+    for root, _, files in os.walk(dataset.location):
+        for file in files:
+            if file.endswith('.coco.json'):
+                COCO_PATH = (os.path.join(root, file))
+
+    class_ids = list_all_labels(COCO_PATH)
+
+    return COCO_PATH, class_ids
+
+
+def init_from_folder(folder_path):
+    """
+    Initialize from a folder containing images and COCO-style annotations.
+
+    Inputs:
+        folder_path: path to folder containing images and COCO-style annotation.
+    Returns:
+        COCO_PATH: path to annotations file
+        class_ids: a list of integers representing each unique class in the dataset segmentations
+    """
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith('.coco.json'):
+                COCO_PATH = (os.path.join(root, file))
+                 
+    with open(COCO_PATH, 'r') as f:
+        coco = json.load(f)
+
+    # Get all class ids from segmentation annotations
+    class_ids = {ann['category_id'] for ann in coco['annotations']}
+
+    # Count unique classes
+    num_classes = len(class_ids)
+    print(f"Total number of unique classes: {num_classes}")
+
+    return COCO_PATH, class_ids
+
+def get_image(image_name, parent_directory):
+    """
+    Inputs:
+        image_name: full or partial image file name.
+        parent_directory: folder that contains the image. Recursive folders are ok.
+    Returns:
+        image: the image with matching file name.
+    """
+    for root, _, files in os.walk(parent_directory):
+        for file in files:
+            if image_name in file:
+                return os.path.join(root, file)
+    
+    raise FileNotFoundError(f"No matching images for {image_name} were found under {parent_directory}.")
+
+def get_mask(path_to_image, COCO_PATH, class_id):
+    """
+    Inputs: 
+        path_to_image: full image filepath
+        class_id: integer representing the class to get the segmentation mask for
+        COCO_PATH: full path to COCO annotations file
+    Returns:
+        mask: binary mask for the image and class
+    """
+    file_name = os.path.basename(path_to_image)
+
+    with open(COCO_PATH, 'r') as f:
+        coco = json.load(f)
+
+    image_id_map = {img["file_name"]: img["id"] for img in coco["images"]}
+    image_info_map = {img["id"]: img for img in coco["images"]}
+    annotations_per_image = defaultdict(list)
+    for ann in coco["annotations"]:
+        annotations_per_image[ann["image_id"]].append(ann)
+
+    if file_name not in image_id_map:
+        raise ValueError(f"Image {file_name} not found in COCO annotations.")
+
+    image_id = image_id_map[file_name]
+    image_info = image_info_map[image_id]
+    height, width = image_info["height"], image_info["width"]
+
+    # Create blank mask
+    mask = np.zeros((height, width), dtype=np.uint8)
+
+    for ann in annotations_per_image[image_id]:
+        if ann["category_id"] != class_id:
+            continue
+        
+        segmentation = ann["segmentation"]
+        if ann.get("iscrowd", 0):
+            rle = mask_utils.frPyObjects([segmentation], height, width)
+            m = mask_utils.decode(rle[0])
+        else:
+            rle = mask_utils.frPyObjects(segmentation, height, width)
+            m = mask_utils.decode(rle)
+            if len(m.shape) == 3:
+                m = np.any(m, axis=2).astype(np.uint8)
+        
+        mask[m.astype(bool)] = 1  # Binary mask
+
+    return mask
---
+++ propagate_mask_medsam2.py
@@ -0,0 +1,226 @@
+import numpy as np
+import os
+from torch.nn.functional import sigmoid
+
+from sam2.build_sam import build_sam2_video_predictor
+import import_data_from_roboflow
+
+class CustomMEDSAM2():
+    def __init__(self, config_filepath, checkpoint_filepath):
+        self.config_filepath = config_filepath
+        self.checkpoint_filepath = checkpoint_filepath
+
+    def initialize_segmentation_model(self):
+        """
+        Initialize predictor for segmentation.
+        Inputs:
+            config_filepath: file path to configuration file.
+            checkpoint_filepath: file path to pre-trained checkpoint to load.
+        Returns:
+            predictor: MEDSAM2 predictor
+        """
+        # Initialize predictor
+        predictor = build_sam2_video_predictor(
+            config_file= self.config_filepath,
+            ckpt_path= self.checkpoint_filepath,
+            apply_postprocessing=True,
+            # hydra_overrides_extra=hydra_overrides_extra,
+            vos_optimized=  True,
+        )
+        return predictor
+    
+    def _propagate_forward(self, images_to_segment_path, start_mask, start_keyframe_idx, end_keyframe_idx, class_id):
+        """
+        Forward pass for segmentation prediction given a start mask and end point.
+        """
+        predictor = self.initialize_segmentation_model()
+        inference_state = predictor.init_state(video_path=images_to_segment_path, async_loading_frames=False)
+
+        video_logits_f= {}
+        video_segments_f = {}
+
+        predictor.add_new_mask(
+            inference_state=inference_state,
+            frame_idx=start_keyframe_idx,
+            obj_id=class_id,
+            mask=start_mask,
+                )
+        
+        for out_frame_idx, out_obj_ids, out_mask_logits in predictor.propagate_in_video(inference_state, start_frame_idx=start_keyframe_idx,
+            max_frame_num_to_track=end_keyframe_idx-start_keyframe_idx):
+            # Get binary masks
+            per_obj_output_mask = {
+                out_obj_id: (out_mask_logits[i] > 0.0).cpu().numpy()
+                for i, out_obj_id in enumerate(out_obj_ids)
+            }
+
+            # Save segmentation
+            video_segments_f[out_frame_idx] = per_obj_output_mask
+
+            # Store logits
+            video_logits_f[out_frame_idx] = {
+                out_obj_id: out_mask_logits[i].cpu()
+                for i, out_obj_id in enumerate(out_obj_ids)
+            }
+
+            # Feed each predicted mask as prompt for the next frame
+            next_frame_idx = out_frame_idx + 1
+            if next_frame_idx < end_keyframe_idx:
+                for obj_id, mask in per_obj_output_mask.items():
+                    if mask is not None and np.any(mask):
+                        predictor.add_new_mask(
+                            inference_state=inference_state,
+                            frame_idx=next_frame_idx,
+                            obj_id=obj_id,
+                            mask=mask.squeeze(),
+                        )
+            return video_segments_f, video_logits_f
+        
+
+    
+    def _propagate_backward(self, images_to_segment_path, end_mask,  start_keyframe_idx, end_keyframe_idx, class_id):
+        """
+        Backwards pass for segmentation prediction given a start mask and end point.
+        """
+        predictor = self.initialize_segmentation_model()
+        inference_state = predictor.init_state(video_path=images_to_segment_path, async_loading_frames=False)
+
+        video_logits_b= {}
+        video_segments_b = {}
+
+        # switch start and end idx if start is greater than end
+        if start_keyframe_idx > end_keyframe_idx:
+            tmp = end_keyframe_idx
+            end_keyframe_idx = start_keyframe_idx
+            start_keyframe_idx = tmp
+
+        predictor.add_new_mask(
+            inference_state=inference_state,
+            frame_idx=end_keyframe_idx,
+            obj_id=class_id,
+            mask=end_mask,
+                )
+        
+        for out_frame_idx, out_obj_ids, out_mask_logits in predictor.propagate_in_video(inference_state, start_frame_idx=end_keyframe_idx,
+            max_frame_num_to_track=end_keyframe_idx-start_keyframe_idx, reverse=True):
+            # Get binary masks
+            per_obj_output_mask = {
+                out_obj_id: (out_mask_logits[i] > 0.0).cpu().numpy()
+                for i, out_obj_id in enumerate(out_obj_ids)
+            }
+
+            # Save segmentation
+            video_segments_b[out_frame_idx] = per_obj_output_mask
+
+            # Store logits
+            video_logits_b[out_frame_idx] = {
+                out_obj_id: out_mask_logits[i].cpu()
+                for i, out_obj_id in enumerate(out_obj_ids)
+            }
+
+            # Feed each predicted mask as prompt for the next frame
+            next_frame_idx = out_frame_idx - 1
+
+            if next_frame_idx < end_keyframe_idx:
+                for obj_id, mask in per_obj_output_mask.items():
+                    if mask is not None and np.any(mask):
+                        predictor.add_new_mask(
+                            inference_state=inference_state,
+                            frame_idx=next_frame_idx,
+                            obj_id=obj_id,
+                            mask=mask.squeeze(),
+                        )
+        return video_segments_b, video_logits_b
+
+
+    def propagate_sequence(self, images_to_segment_path, class_id, coco_path):
+        """
+        Propagate masks from start_idx to end_idx (inclusive) using MedSAM2, for specified class.
+        Inputs:
+            images_to_segment_path: path to directory of image stack
+            class_id: COCO category id for segmentation
+            coco_path: COCO annotation file path 
+        Returns:
+            fused_masks (dict): Predicted mask for each image
+        """
+
+        frame_names = import_data_from_roboflow.list_all_images(images_to_segment_path)
+        keyframe_indices = import_data_from_roboflow.get_keyframe_indices(coco_path, images_to_segment_path, class_id)
+        num_frames = len(frame_names)
+
+        start_idx = 0
+        end_idx = num_frames - 1
+
+        video_segments_f = {}
+        video_logits_f = {}
+        video_segments_b = {}
+        video_logits_b = {}
+
+        for i in range(0, len(keyframe_indices)-1):
+            start_idx = keyframe_indices[i]
+            end_idx = keyframe_indices[i+1]
+
+            start_mask = import_data_from_roboflow.get_mask(os.join(images_to_segment_path,frame_names[start_idx]), coco_path, class_id)
+            start_mask = np.array(start_mask).astype(np.uint8)
+
+            # forward pass
+            segments, logits = self._propagate_forward(images_to_segment_path, start_mask, start_idx, end_idx, class_id)
+            video_segments_f.update(segments)
+            video_logits_f.update(logits)
+
+            end_mask = import_data_from_roboflow.get_mask(os.join(images_to_segment_path,frame_names[end_idx]), coco_path, class_id)
+            end_mask = np.array(start_mask).astype(np.uint8)
+
+            # backwards
+            segments, logits = self._propagate_backward(images_to_segment_path, end_mask, start_idx, end_idx, class_id)
+            video_segments_b.update(segments)
+            video_logits_b.update(logits)
+
+        fused_masks = self.merge_bidirectional_masks([start_idx, end_idx], video_logits_f, video_logits_b)
+
+        return fused_masks
+
+
+    def merge_bidirectional_masks(self, keyframe_indices, video_logits_f, video_logits_b, thresh=0.5):
+
+        fused_masks = {}
+
+        for t in range(keyframe_indices[0], keyframe_indices[1] + 1):
+            if t in video_logits_f and t in video_logits_b:
+                # print(f"Merging masks for frame {t}")
+                frame_logits_f = video_logits_f[t]
+                frame_logits_b = video_logits_b[t]
+                fused_masks[t] = {}
+
+                for obj_id in frame_logits_f.keys():
+                    if obj_id in frame_logits_b:
+                        logit_f = frame_logits_f[obj_id]
+                        logit_b = frame_logits_b[obj_id]
+
+                        # Distance-weighted fusion: favor closer keyframe
+                        alpha = (keyframe_indices[1] - t) / (keyframe_indices[1] - keyframe_indices[0])
+                        fused_logit = alpha * logit_f + (1 - alpha) * logit_b
+
+                        # Apply sigmoid to get probabilities
+                        prob = sigmoid(fused_logit)
+
+                        # Threshold to bool
+                        mask = (prob > thresh).bool().numpy()
+
+                        fused_masks[t][obj_id] = mask
+            elif t in video_logits_f:
+                print(f"Using forward masks for frame {t}")
+                if t not in fused_masks:
+                    fused_masks[t] = {}
+                for obj_id, logit in video_logits_f[t].items():
+                    fused_masks[t][obj_id] = (sigmoid(logit) > thresh).bool().numpy()
+            elif t in video_logits_b:
+                print(f"Using backward masks for frame {t}")
+                if t not in fused_masks:
+                    fused_masks[t] = {}
+                for obj_id, logit in video_logits_b[t].items():
+                    fused_masks[t][obj_id] = (sigmoid(logit) > thresh).bool().numpy()
+            else:
+                print(f"No masks found for frame {t}")
+
+        return fused_masks
