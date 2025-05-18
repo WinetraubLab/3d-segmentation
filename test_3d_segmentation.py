2e09a80ccb3a654832236436f69a2278548ad141
zerlinazl <76027329+zerlinazl@users.noreply.github.com>
2025-05-18 08:29:13 -0700

    add testers

--- test_3d_segmentation.py
+++ test_3d_segmentation.py
@@ -9,9 +9,24 @@ class TestFitPlaneElastic(unittest.TestCase):
 
     def setUp(self):
         np.random.seed(42)
+        self.original_images_path = "test_vectors/sample_image_sequence/"
+        self.images_to_segment_path = "test_vectors/preprocessed_images/"
+        self.workspace_name = "yolab-kmmfx"  
+        self.project_name = "vol1_2"
+        self.api_key = "" # Set when testing test_init_roboflow()
+        self.MODEL_CONFIG = "configs/sam2.1_hiera_t512.yaml"
+        self.MODEL_CHECKPOINT = "checkpoints/MedSAM2_latest.pt"
+
+    def set_up_global_vars(self):
+        import_data_from_roboflow.init_from_folder("test_vectors/sample_annotations_folder/")
+        import_data_from_roboflow.preprocess_images(self.original_images_path, self.images_to_segment_path)
         
-    def test_main_function_runs(self):
+    def test_init_folder(self):
         class_ids = import_data_from_roboflow.init_from_folder("test_vectors/sample_annotations_folder/")
+        assert len(class_ids) == 3
+    
+    def test_init_roboflow(self):
+        class_ids = import_data_from_roboflow.init_from_roboflow(self.workspace_name, self.project_name, self.api_key)
         assert len(class_ids) == 3
     
     def test_list_images_masks(self):
@@ -19,4 +34,24 @@ class TestFitPlaneElastic(unittest.TestCase):
         assert len(import_data_from_roboflow.list_all_labels()) == 3
 
     def test_preprocess_imgs(self):
-        import_data_from_roboflow.preprocess_images("test_vectors/sample_image_sequence/", "test/vectors/preprocessed_images/")
+        import_data_from_roboflow.preprocess_images(self.original_images_path, self.images_to_segment_path)
+        fpath = import_data_from_roboflow.get_images_to_segment_path()
+        assert fpath == self.images_to_segment_path
+
+    def test_list_all_images(self):
+        self.set_up_global_vars()
+        image_names = import_data_from_roboflow.list_all_images()
+        assert len(image_names) == 6
+
+    def test_get_kf_indices(self):
+        self.set_up_global_vars()
+        kf_indices = import_data_from_roboflow.get_keyframe_indices(0)
+        assert kf_indices[0] == 0
+        assert kf_indices[1] == 5
+    
+    def test_get_image(self):
+        import_data_from_roboflow.get_image("y0124_png.rf.dbeef95400a14da233736fc6e7df31b9.jpg")
+
+    def test_get_mask(self):
+        m = import_data_from_roboflow.get_mask("y0124_png.rf.dbeef95400a14da233736fc6e7df31b9.jpg", 0)
+        assert np.any(np.array(m))
