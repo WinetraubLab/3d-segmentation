63b9d209414a1798cf0046760990d81497d8c604
zerlinazl <76027329+zerlinazl@users.noreply.github.com>
2025-06-22 22:11:01 -0700

    update test functions

--- test_3d_segmentation.py
+++ test_3d_segmentation.py
@@ -20,6 +20,8 @@ class Test3dSegmentation(unittest.TestCase):
     def set_up_global_vars(self):
         import_data_from_roboflow.init_from_folder("test_vectors/sample_annotations_folder/")
         import_data_from_roboflow.preprocess_images(self.original_images_path, self.image_dataset_folder_path)
+        self.seg_vol_0 = import_data_from_roboflow.create_mask_volume(0)
+        self.seg_vol_1 = import_data_from_roboflow.create_mask_volume(1)
         
     def test_init_folder(self):
         class_ids = import_data_from_roboflow.init_from_folder("test_vectors/sample_annotations_folder/")
@@ -63,14 +65,14 @@ class Test3dSegmentation(unittest.TestCase):
     def test_forward_pass(self):
         self.set_up_model()
         self.set_up_global_vars()
-        output_masks = self.model._propagate_single_direction(1)
+        output_masks = self.model._propagate_single_direction(self.image_dataset_folder_path, self.seg_vol_1)
         for f in output_masks:
             assert np.any(f)
 
     def test_backward_pass(self):
         self.set_up_model()
         self.set_up_global_vars()
-        output_masks = self.model._propagate_single_direction(1, reverse=True)
+        output_masks = self.model._propagate_single_direction(self.image_dataset_folder_path, self.seg_vol_1, reverse=True)
         for f in output_masks:
             assert np.any(f)
 
@@ -88,21 +90,21 @@ class Test3dSegmentation(unittest.TestCase):
     def test_propagate(self):
         self.set_up_model()
         self.set_up_global_vars()
-        output_masks = self.model.propagate(1)
+        output_masks = self.model.propagate(self.image_dataset_folder_path, self.seg_vol_1)
         for f in output_masks:
             assert np.any(f)
 
     def test_propagate_with_smoothing(self):
         self.set_up_model()
         self.set_up_global_vars()
-        output_masks = self.model.propagate(1, 3, 5)
+        output_masks = self.model.propagate(self.image_dataset_folder_path, self.seg_vol_1, 1, 3, 5)
         for f in output_masks:
             assert np.any(f)
     
     def test_combine_class_masks(self):
         self.set_up_model()
         self.set_up_global_vars()
-        output_masks0 = self.model.propagate(0)
-        output_masks1 = self.model.propagate(1)
+        output_masks0 = self.model.propagate(self.image_dataset_folder_path, self.seg_vol_0)
+        output_masks1 = self.model.propagate(self.image_dataset_folder_path, self.seg_vol_1)
         output_dir = "test_vectors/output_combined_masks/"
         propagate_mask_medsam2.combine_class_masks([output_masks0, output_masks1], output_dir=output_dir, show=True)
